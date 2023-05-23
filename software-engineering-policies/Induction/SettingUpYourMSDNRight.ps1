c679ffe407096827bd1319c65a0b796ed9905a56
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2023-05-23 23:32:20 +0100

    Create SettingUpYourMSDNRight.ps1

---
+++ software-engineering-policies/Induction/SettingUpYourMSDNRight.ps1
@@ -0,0 +1,18 @@
+# Setting up your MSDN right.
+
+You may be purchased an MSDN license ranging from MSDN Platforms up to Enterprise, depending on your needs. One of the benefits of an MSDN license is linked access to Azure Services and the option to spend monthly azure credits.
+
+One of the main tools we use for source control is GitHub and Workflow using Azure DevOps.
+
+MSDN + GitHub will be purchased for anyone with Pro and Enterprise Licenses.
+
+## Setup for GitHub
+
+In order to leverage the MSDN license against your github profile, you can do one of two things:
+
+- The first is to create a github account linked to your work email, your MSDN is linked to your work email, so once the two find each other your will instantly be upgraded.
+- The second option is to add your work email as an extra email on your [github profile](https://github.com/settings/emails)
+
+## Setup for Azure DevOps
+
+Your MSDN license gives you acccess to any Azure DevOps organisations, this works against your work email to begin with. browsing to [https://my.visualstudio.com/subscriptions](https://my.visualstudio.com/subscriptions), provides the option of adding an alternative account. This should primarily be your admin account, doing this will allow you to leverage the same benefits against that account.
